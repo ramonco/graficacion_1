140c605f7bf8399423ce198e5ac3602e3ed02002
Luis Fernano Lozano Martinez <fernando.lozano.0506@gmail.com>
2018-09-05 11:54:06 -0500

    Commit 2

--- JavaConsoleApplication/src/javaconsoleapplication/JavaConsoleApplication.java
+++ JavaConsoleApplication/src/javaconsoleapplication/JavaConsoleApplication.java
@@ -1,9 +1,11 @@
+package javaconsoleapplication;
+
+import java.util.Scanner;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaconsoleapplication;
 
 /**
  *
@@ -17,6 +19,8 @@ public class JavaConsoleApplication {
     public static void main(String[] args) {
         // TODO code application logic here
         SALUDO();
+        
+        dihola();
     }
     
     public static void SALUDO () {
@@ -34,5 +38,11 @@ public class JavaConsoleApplication {
     
     public static void Saludo3 (){
         System.out.println("Hola!!");
+    }
+    
+    public static void dihola (){
+    
+    System.out.println("Soy el dick");
+        
     }
 }
